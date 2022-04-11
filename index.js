c164d300b7edd1f51f8f0d0ac8c5e93019e977eb
CatherineHonors <cathonors@gmail.com>
2022-04-11 13:27:16 -0400

    commit tree da8cc2f51c21be2cfdc6fee5e7af290e85413d26

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+const ptag = document.getElementById('text');
+
+// console.log(ptag);
+
+// document.addEventListener('DOMContentLoaded', () => ptag.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.')
+
+const beLoaded = e => {
+    ptag.innerHTML = 'This is really cool'
+}
+
+document.addEventListener('DOMContentLoaded', beLoaded)
